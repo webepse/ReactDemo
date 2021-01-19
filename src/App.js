e4e408c5ebeccc4edc574972ca168c00ee441afe
Boderio <boderiotrigan@hotmail.com>
2021-01-19 11:53:22 +0100

    le state

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import React, {Component, Fragment} from 'react';
 import './App.css'
 import Membre from './components/Membre'
 
+const league = {
+  membre1: {
+    nom: 'Batman',
+    age: 45
+  },
+  membre2: {
+    nom: 'Superman',
+    age: 45
+  },
+  membre3: {
+    nom: 'Wonder Woman',
+    age: 79
+  },
+  membre4: {
+    nom: 'Catwoman',
+    age: 30
+  }
+}
 class App extends Component {
-
+  state = {
+    league : league 
+  }
   render() { 
     const {title} = this.props
     return(
@@ -14,13 +34,10 @@ class App extends Component {
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut recusandae adipisci quaerat quae, eveniet voluptatem voluptates atque libero voluptate necessitatibus beatae rerum hic debitis cum, deserunt dolorem doloremque nemo error modi molestias rem ex quisquam illum! Accusantium, molestiae? Error corporis, excepturi cumque incidunt quam veritatis sint molestias deleniti ipsam fugit ipsum amet laborum omnis illo est voluptas, eveniet eum ducimus, earum obcaecati. Quo cum earum quae, quos ipsa, facere veniam numquam ipsum, ullam voluptate assumenda esse iusto accusamus laborum! Sint quod commodi assumenda voluptatibus, ex nulla velit ducimus accusantium tempore ab a architecto quasi provident nisi tempora doloremque culpa ut?</p>
           </div>
           <div>test</div>
-          <Membre nom="Batman" />
-          <Membre nom="Catwoman" />
-          <Membre nom="Superman" />
-          <Membre nom="Wonder Woman" />
-          <Membre nom="Ace">
-            Je suis le Batdog
-          </Membre>
+          <Membre nom={this.state.league.membre1.nom} />
+          <Membre nom={this.state.league.membre2.nom} />
+          <Membre nom={this.state.league.membre3.nom} />
+          <Membre nom={this.state.league.membre4.nom} />       
         </Fragment>
       //React.createElement('div', {className: 'App'}, React.createElement('h1',null,'React App'))
     )
