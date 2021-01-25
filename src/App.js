f99b21fad0263ceeb39b4b206a04a6ce22f92038
berti <berti@CLASSES.LOCAL>
2021-01-25 11:37:09 +0100

    gestion des input + style + object.keys

--- src/App.js
+++ src/App.js
@@ -32,9 +32,23 @@ class App extends Component {
     this.setState({league:league})
   }
 
+  handleChange = (event) => {
+    const league = {...this.state.league}
+    const nom = event.target.value
+    league.membre1.nom = nom
+    this.setState({league:league})
+  }
+
   render() { 
     const {title} = this.props
     const nb = 5
+    const liste = Object.keys(this.state.league).map(membre => {
+      return (
+        <Membre key={membre} age={this.state.league[membre].age} nom={this.state.league[membre].nom} />
+      )
+    })
+
+
     return(
         <Fragment>
           <div className="App">
@@ -43,6 +57,10 @@ class App extends Component {
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut recusandae adipisci quaerat quae, eveniet voluptatem voluptates atque libero voluptate necessitatibus beatae rerum hic debitis cum, deserunt dolorem doloremque nemo error modi molestias rem ex quisquam illum! Accusantium, molestiae? Error corporis, excepturi cumque incidunt quam veritatis sint molestias deleniti ipsam fugit ipsum amet laborum omnis illo est voluptas, eveniet eum ducimus, earum obcaecati. Quo cum earum quae, quos ipsa, facere veniam numquam ipsum, ullam voluptate assumenda esse iusto accusamus laborum! Sint quod commodi assumenda voluptatibus, ex nulla velit ducimus accusantium tempore ab a architecto quasi provident nisi tempora doloremque culpa ut?</p>
           </div>
           <div>test</div>
+          <input 
+          value={this.state.league.membre1.nom}
+          onChange={this.handleChange}
+          type="text"/>
           <Membre 
           age={this.state.league.membre1.age}
           nom={this.state.league.membre1.nom} />
@@ -63,6 +81,8 @@ class App extends Component {
           <Button 
           nb={nb}
           vieillir={()=> this.handleClick(nb)} />
+          <h1>Rendu avec boucle</h1>
+          {liste}
         </Fragment>
       //React.createElement('div', {className: 'App'}, React.createElement('h1',null,'React App'))
     )
